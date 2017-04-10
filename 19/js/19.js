443197e4907719489c7c2f43c02f1b8e4fa92f7b
leleTang <623040755@qq.com>
2017-04-10 17:25:59 +0800

    19修改

--- 19/js/19.js
+++ 19/js/19.js
@@ -105,14 +105,36 @@ function numbers() {
 	}
 
 }
+//冒泡排序参考
+//var arr = [3, 2, 4, 9, 1, 5, 7, 6, 8];
+//var arrSorted = bubbleSort(arr);
+//function bubbleSort(arr) {
+//  var i = arr.length, j;
+//  var tempExchangVal;
+//  while (i > 0) {
+//      for (j = 0; j < i - 1; j++) {
+//          if (arr[j] > arr[j + 1]) {
+//              tempExchangVal = arr[j];
+//              arr[j] = arr[j + 1];
+//              arr[j + 1] = tempExchangVal;
+//          }
+//      }
+//      i--;
+//  }
+//  return arr;
+//}
+// 
+//console.log(arrSorted);
+//alert(arrSorted);
 //排序
-function sort(){
-	var contentDivHeight=[];
-	var contentChildNode=contentFrame.childNodes;
-	for (var i=0 ;i<contentChildNode.length;i++) {
-		console.log(contentChildNode[i].innerHTML);
-		
+function sort() {
+	var contentDivHeight = [];
+	var contentChildNode = contentFrame.childNodes;
+	//	先取出div里的值
+	for(var i = 0; i < contentChildNode.length; i++) {
+		contentDivHeight[i] = contentChildNode[i].innerHTML;
 	}
+
 }
 //全局变量
 var contentFrame = document.getElementsByClassName("content-frame")[0];
