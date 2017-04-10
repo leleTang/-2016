71a4f2d1d76a3f9a15f7f731402de64541764c7e
leleTang <623040755@qq.com>
2017-04-10 11:42:11 +0800

    移出时有时要点两次的bug

--- 18/js/18.js
+++ 18/js/18.js
@@ -1,34 +1,79 @@
-var operation = document.getElementById("operation");
-var operationButton=operation.getElementsByTagName("button");
-var a="";
-for(var i = 0; i < operationButton.length; i++) {
-	a.onclick=operationButton[i].value;
-}
-console.log(a)
+function operarion() {
+	var operation = document.getElementById("operation");
+	var operationButton = operation.getElementsByTagName("button");
 
+	var operationContent = "";
+	for(var i = 0; i < operationButton.length; i++) {
+		operationButton[i].onclick = function() {
+			operationContent = this.value;
+			switch(operationContent) {
+				case "leftInto":
+					leftInto();
+					break;
+				case "rightInto":
+					rightInto();
+					break;
+				case "leftOut":
+					leftOut();
+					break;
+				case "rightOut":
+					rightOut();
+					break;
+			}
+		}
 
+	}
 
-function leftOut() {
-	var leftOut = document.getElementById("operation-leftOut-button");
-
-}
-
-function rightOut() {
-	var rightOut = document.getElementById("operation-RightOut-button");
 }
 
 function leftInto() {
-	var leftInto = document.getElementById("operation-leftInto-button");
+	var operationInputValue = document.getElementById("operation-input").value;
+	var contentFirstChild = contentFrame.firstChild;
+	if(operationInputValue == "") {
+		alert("要输入数据哦~");
+	} else {
+		var contentDiv = document.createElement("div");
+		contentDiv.innerHTML = operationInputValue;
+		contentFrame.insertBefore(contentDiv, contentFirstChild);
+		console.log(operationInputValue)
+	}
 }
 
-function rightInit() {
-	var rightInto = document.getElementById("operatin-rightInto-button");
+function rightInto() {
+	var operationInputValue = document.getElementById("operation-input").value;
+	var contentFirstChild = contentFrame.lastChild;
+	if(operationInputValue == "") {
+		alert("要输入数据哦~");
+	} else {
+		var contentDiv = document.createElement("div");
+		contentDiv.innerHTML = operationInputValue;
+		contentFrame.insertBefore(contentDiv, contentFirstChild);
+	}
 }
+
+function leftOut() {
+	var operationInputValue = document.getElementById("operation-input").value;
+	if(contentFrame.childNodes.length<=0){
+		alert("没有东西可以删啦~");
+	}else{
+		var contentFirstChild = contentFrame.firstChild;
+		contentFrame.removeChild(contentFirstChild);
+	}
+}
+
+function rightOut() {
+	var operationInputValue = document.getElementById("operation-input").value;
+	if(contentFrame.childNodes.length<=0){
+		alert("没有东西可以删啦~");
+	}else{
+		var contentFirstChild = contentFrame.lastChild;
+		contentFrame.removeChild(contentFirstChild);
+	}
+}
+//全局变量
+	var contentFrame = document.getElementsByClassName("content-frame")[0];
 //初始化函数
 function init() {
-	leftOut();
-	rightOut();
-	leftInto();
-	rightInit();
+	operarion();
 }
 init();
