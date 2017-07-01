42cb08ebb1c081842c31cf540896df3ebb7f14b6
leleTang <623040755@qq.com>
2017-07-01 12:44:25 +0800

    21完成一个小功能
    Signed-off-by: leleTang <623040755@qq.com>

--- 21/js/21.js
+++ 21/js/21.js
@@ -1,38 +1,37 @@
 /************************************页面初始化开始***********************************/
 window.onload = function() {
-	var a = document.getElementById("a");
-	var tagInput = document.getElementById("tagInput"); //tag输入框
-	a.onclick = function() {
-		addTag()
-	}
 
 }
 /************************************页面初始化结束***********************************/
 /*******常量开始**********/
-var tagList = new Array();
-var number = 0;
+
 /*******常量结束**********/
 /*******函数开始**********/
+//键盘响应事件
+function check() {
+	var key = event.keyCode;
+	if(key == 13 || key == 188 || key == 32) {
+		addTag();
+	}
+}
+
 function addTag() {
 	var tagInput = document.getElementById("tagInput"); //tag输入框
 	var tagContent = document.getElementById("tagContent"); //tag内容框
 	var tagSpan = document.createElement("span");
 	tagSpan.className = "tagSpan";
-	console.log()
-	if(tagInput.value == "") {
+	tagSpan.setAttribute("data-tooltip","删除");
+	if(tagInput.value == null || tagInput.value == undefined || tagInput.value == '') {
 		alert("请输入有效值")
 	} else {
 		if(tagContent.childNodes.length <= 10) {
-			tagList[number] = tagInput.value;
-			tagSpan.innerHTML = tagList[number];
+			tagSpan.innerHTML = tagInput.value;
 			tagContent.prepend(tagSpan);
-			number++;
 		}
 		if(tagContent.childNodes.length > 10) {
-			console.log(tagList)
-			tagList.pop();
 			tagContent.removeChild(tagContent.lastChild);
 		}
 	}
+	tagInput.value = null;
 }
 /*******函数结束**********/
